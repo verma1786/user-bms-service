f0f5c22cc845c9aa17fa74fb425f1d6cd576a6a4
vinay <vinay.verma107>
2022-09-13 15:27:16 +0530

    Initial commit

---
+++ src/main/java/com/bms/UserBmsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBmsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserBmsServiceApplication.class, args);
+	}
+
+}
